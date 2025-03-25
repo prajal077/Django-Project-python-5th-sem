21d0a611287cd0fe5fed6c2d19ea336170b86c4e
Prajal <prajalbhattarai1627@gmail.com>
2025-03-25 17:43:08 +0545

    added contents in models.py, admin.py also installed pillow

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Blog
+from .models import category
+
+
+admin.site.register(Blog)
+admin.site.register(category)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class category(models.Model) :
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    image = models.ImageField(upload_to="category")
+
+    def __str__(self) :
+        return self.title
+    
+class Blog(models.Model) :
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    image = models.ImageField(upload_to="Blog")
+    category = models.ForeignKey(category, on_delete = models.CASCADE)
+    user = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self) :
+        return self.title
